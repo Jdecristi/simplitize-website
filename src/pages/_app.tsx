f4e47ff6e308ae763a763efb3d104eabc2e338ca
Josh DeCristi <joshdecristi@Joshs-MacBook-Pro.local>
2023-06-10 23:43:56 -0400

    feat: customize eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { type AppType } from "next/app";
-import { api } from "@src/utils/api";
-import "@src/styles/globals.css";
+import { type AppType } from 'next/app';
+import { api } from '@src/utils/api';
+import '@src/styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
